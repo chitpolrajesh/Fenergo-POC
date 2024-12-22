b4995eb129810ea812e00300eb0e2477f697fffb
Rajesh-chitpol-vistra <124994078+chitpolrajesh@users.noreply.github.com>
2024-12-22 05:53:17 -0500

    Role Type

---
+++ Object Repository/Page_vistra-sdlc-uat - Fenergo/RoleType_DropdownArrow.rs
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>RoleType_DropdownArrow</name>
+   <tag></tag>
+   <elementGuidId>5a6139ab-7fb5-4b3d-b76b-102e6239ee27</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.jou99.jou234.jou223.jou206 > span.jou242 > svg > path</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>[data-testid=&quot;fen-dropdown-role-type&quot;] >> internal:label=&quot;Open&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>path</value>
+      <webElementGuid>48cfc0aa-2311-4c44-8717-7d41b437b93a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>d</name>
+      <type>Main</type>
+      <value>M8.414 9.5h7.172a1 1 0 0 1 .707 1.707l-3.586 3.586a1 1 0 0 1-1.414 0l-3.586-3.586A1 1 0 0 1 8.414 9.5Z</value>
+      <webElementGuid>f83eb7c7-d590-4c7a-9fb2-813ff2904b59</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>fill</name>
+      <type>Main</type>
+      <value>#557391</value>
+      <webElementGuid>6c170a82-1597-498c-833d-a4480bfa824e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>fill-rule</name>
+      <type>Main</type>
+      <value>evenodd</value>
+      <webElementGuid>785e5959-d0e3-48bb-8afa-7742c0ff2747</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;single-spa-application:@fenx/ui-journey&quot;)/div[1]/div[1]/div[1]/div[1]/div[1]/div[@class=&quot;jou26&quot;]/div[@class=&quot;jou29 jou30&quot;]/div[@class=&quot;jou130 jou131&quot;]/div[1]/div[@class=&quot;jou580&quot;]/div[@class=&quot;jou210 jou202 jou211 jou216 jou215&quot;]/div[1]/div[@class=&quot;jou161 jou160 jou163 jou164&quot;]/div[@class=&quot;jou180 jou165 jou217 jou203 jou207 jou190 jou181 jou185 jou170 jou187 jou172&quot;]/div[@class=&quot;jou220&quot;]/button[@class=&quot;jou99 jou234 jou223 jou206&quot;]/span[@class=&quot;jou242&quot;]/svg[1]/path[1]</value>
+      <webElementGuid>f257a238-b8e7-4cc2-b3c0-5b52b8de01a9</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
